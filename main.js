6b10d06d22af51427fe612981f152631e528f72e
davidepatruno <patrunodavid@gmail.com>
2018-12-11 16:48:14 +0100

    quadrati che si colorano a seconda del numero maggiore o minore di 5 OK

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
+  var quadrato = '<div class="quadrato"></div>';
   for (var i=0; i < 36 ; i++){
-    var quadrato = '<div class="quadrato"></div>';
     $('.wrapper_griglia').append(quadrato);
+
   }
+  $('.quadrato').click(function(){
+    var thisquadrato = $(this);
+    var numero = "";
+    $.ajax({
+      url: 'https://www.boolean.careers/api/random/int',
+      method: 'GET',
+      success: function(data)
+      {
+        var numero = data.response;
+        console.log(data);
+        if (numero <= 5) {
+          $(thisquadrato).addClass('verde');
+        }
+        else {
+          $(thisquadrato).addClass('yellow');
+        }
+      },
+      error: function()
+      {
+        alert("non funzia");
+      },
+    });
+  })
 })
